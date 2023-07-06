ca2f74172829f56e96b9b012dfb248ea2529d192
Clauson Silva <clausoncarvalho@hotmail.com>
2023-07-06 15:05:22 +0200

    Small changes in the api code and notebook to adapt to new structure

--- gcnb_pkg/api/fast_get.py
+++ gcnb_pkg/api/fast_get.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
 #add import of processing function
-from gcnb_pkg.preprocessing.encoding_pipeline import processing_pipe
+#from gcnb_pkg.preprocessing.encoding_pipeline import processing_pipe
 
 app = FastAPI()
 
@@ -22,38 +22,38 @@ app.add_middleware(
 
 @app.get("/predict")
 def predict_features(
-    usetype_bl: str,
+    usetype_block: str,
     district: str,
     built_type: str,
     residents: float,
-    air_pollut: str,
-    thermal_st: str,
-    social_sta: str,
+    air_pollution: str,
+    thermal_stress: str,
+    social_status: str,
     social_dyn: str,
     rent: float,
-    unemp_bene: float,
+    unemp_benef: float,
     social_hou: float,
     hous_assoc: float,
-    rent_durat: float,
-    aparts_sol: float,
+    rent_duration: float,
+    aparts_sold: float,
     subsidized: bool):
 
 
     input = pd.DataFrame(dict(
-        usetype_bl=[usetype_bl],
+        usetype_block=[usetype_block],
         district=[district],
         built_type=[built_type],
         residents=[residents],
-        air_pollut=[air_pollut],
-        thermal_st=[thermal_st],
-        social_sta=[social_sta],
+        air_pollution=[air_pollution],
+        thermal_stress=[thermal_stress],
+        social_status=[social_status],
         social_dyn=[social_dyn],
         rent=[rent],
-        unemp_bene=[unemp_bene],
+        unemp_benef=[unemp_benef],
         social_hou=[social_hou],
         hous_assoc=[hous_assoc],
-        rent_durat=[rent_durat],
-        aparts_sol=[aparts_sol],
+        rent_duration=[rent_duration],
+        aparts_sold=[aparts_sold],
         subsidized=[subsidized]))
 
 
